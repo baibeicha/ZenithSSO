b571c23a36a14e3adb1d08deba3c90474fc284ed
Konstantin <myvvot@gmail.com>
2026-05-14 10:39:43 +0300

    Created configs

---
+++ configs/datasource.go
@@ -0,0 +1,19 @@
+package configs
+
+type Datasource struct {
+	Url      string
+	DB       string
+	User     string
+	Password string
+	SslMode  string
+}
+
+func NewDatasourceFromConfig(cfg *Config) *Datasource {
+	return &Datasource{
+		Url:      cfg.GetString("datasource.url"),
+		DB:       cfg.GetString("datasource.db"),
+		User:     cfg.GetString("datasource.user"),
+		Password: cfg.GetString("datasource.password"),
+		SslMode:  cfg.GetString("datasource.ssl_mode"),
+	}
+}
